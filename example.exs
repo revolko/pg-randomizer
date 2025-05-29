e3cbffa5faf8129afed095d27c34bdc9e1edb36f
Juraj Paluba <juraj.palubajp@gmail.com>
2025-05-29 19:19:42 +0200

    example: extend example with postgres integration
    The docker-compose file is only for example purposes (not production
    settings). The `init.sql` script is executed automatically upon creation
    of postgres container.

--- example.exs
+++ example.exs
@@ -1,6 +1,13 @@
 {:ok, queries } = PgRandomizer.generator(10, [
-  %{table_name: "table", columns: ["a", "b"], types: [:integer, :string]},
-  %{table_name: "table2", columns: ["a", "b"], types: [:integer, :string]},
+  %{table_name: "films", columns: ["code", "title"], types: [:integer, :string]},
+  %{table_name: "distributors", columns: ["did", "name"], types: [:integer, :string]},
 ])
 
 Enum.map(queries, fn query -> IO.puts(to_string(query)) end)
+
+{:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "postgres", password: "postgres", database: "postgres")
+
+Enum.map(queries, fn query ->
+  {:ok, res} = Postgrex.query(pid, query)
+  IO.inspect(res)
+end)
---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE films (
+    code        integer NOT NULL,
+    title       varchar(255) NOT NULL
+);
+
+CREATE TABLE distributors (
+     did    integer NOT NULL,
+     name   varchar(255) NOT NULL
+);
